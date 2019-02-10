2862524cbcb7eb3c330b9b4fc5f5d2aabe27c055
Jakob Wolitzki <jakob.wolitzki@gmail.com>
2019-02-10 22:18:47 +0100

    password reset

--- splitit/core/views.py
+++ splitit/core/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 def home(request):
     count = User.objects.count()
@@ -24,3 +27,11 @@ def signup(request):
         'registration/signup.html',
         {'form': form}
     )
+
+@login_required
+def secret_page(request):
+    return render(request, 'secret_page.html')
+
+
+class SecretPage(LoginRequiredMixin, TemplateView):
+    template_name = 'secret_page.html'
--- splitit/splitit/urls.py
+++ splitit/splitit/urls.py
@@ -6,6 +6,8 @@ from core import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
+    path('secret/', views.secret_page, name='secret'),
+    path('secret2/', views.SecretPage.as_view(), name='secret2'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
