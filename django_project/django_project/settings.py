d3b072337b1c6cb0a7a742107498000fa7dfb8cc
Jakob Wolitzki <jakob.wolitzki@gmail.com>
2019-03-05 22:44:54 +0100

    all works

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -132,7 +132,8 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_HOST = 'smtp.gmail.com'
 # EMAIL_PORT = 587
 # EMAIL_USE_TLS = True
